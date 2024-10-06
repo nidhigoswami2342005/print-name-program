18443c0e6d2dbbe5517acca11deb7c8ab15c676b
nidhi <ankit9582240997@gmail.com>
2024-10-06 16:59:44 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,9 @@
+public class Demo{
+    public static void main(String [] args){
+        String name ="nidhi";
+        string name2 = new string(original: nidhi);
+
+        System.out.println(name.equals(name2));
+        System.out .println(name.compareTo(name2));
+    } 
+}
